0cc28d0150d859b8e93e7fb0583a5eb71a773f3f
Gui <guicostaantunes@gmail.com>
2023-04-07 02:00:01 +0100

    Not conflit with Homework

--- lab4/lab4_3/src/test/java/tqs/Lab42aTest.java
+++ lab4/lab4_3/src/test/java/tqs/Lab42aTest.java
@@ -11,6 +11,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import tqs.pages.StartingPage;
+
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.JavascriptExecutor;
@@ -35,46 +38,8 @@ public class Lab42aTest {
 
     @Test
     public void lab42a() {
-        driver.get("https://blazedemo.com/");
-        driver.manage().window().setSize(new Dimension(780, 688));
-        driver.findElement(By.cssSelector("h1")).click();
-        assertThat(driver.getTitle(), is("BlazeDemo"));
-        driver.findElement(By.name("fromPort")).click();
-        {
-            WebElement dropdown = driver.findElement(By.name("fromPort"));
-            dropdown.findElement(By.xpath("//option[. = 'Philadelphia']")).click();
-        }
-        {
-            List<WebElement> elements = driver.findElements(By.name("fromPort"));
-            assert (elements.size() > 0);
-        }
-        driver.findElement(By.name("toPort")).click();
-        {
-            WebElement dropdown = driver.findElement(By.name("toPort"));
-            dropdown.findElement(By.xpath("//option[. = 'Dublin']")).click();
-        }
-        {
-            List<WebElement> elements = driver.findElements(By.cssSelector(".btn-primary"));
-            assert (elements.size() > 0);
-        }
-        driver.findElement(By.cssSelector(".btn-primary")).click();
-        {
-            List<WebElement> elements = driver.findElements(By.cssSelector("tr:nth-child(1) > td:nth-child(7)"));
-            assert (elements.size() > 0);
-        }
-        driver.findElement(By.cssSelector("tr:nth-child(1) .btn")).click();
-        {
-            WebElement element = driver.findElement(By.id("inputName"));
-            Boolean isEditable = element.isEnabled() && element.getAttribute("readonly") == null;
-            assertTrue(isEditable);
-        }
-        driver.findElement(By.id("inputName")).sendKeys("Vicente");
-        driver.findElement(By.id("address")).click();
-        driver.findElement(By.id("address")).sendKeys("Bué Longe");
-        assertFalse(driver.findElement(By.id("rememberMe")).isSelected());
-        driver.findElement(By.id("rememberMe")).click();
-        assertTrue(driver.findElement(By.id("rememberMe")).isSelected());
-        driver.findElement(By.cssSelector(".btn-primary")).click();
-        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Thank you for your purchase today!"));
+
+        StartingPage startingPage = new StartingPage(driver);
+
     }
 }
--- lab4/lab4_3/src/test/java/tqs/pages/StartingPage.java
+++ lab4/lab4_3/src/test/java/tqs/pages/StartingPage.java
@@ -1,13 +1,21 @@
-package test.java.tqs;
+package tqs.pages;
 
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 @ExtendWith(SeleniumJupiter.class)
 public class StartingPage {
 	
+	private final String URL = "https://blazedemo.com/";
+
 	private WebDriver driver;
 
-	@FindBy(id = "inputName")
+	public StartingPage(WebDriver driver) {
+		this.driver = driver;
+
+		driver.get(URL);
+		PageFactory.initElements(driver, this);
+	}
 }
